79132f2db7ee0362cd30b5480d9b004831d8ccc6
zpeng <1208450718@qq.com>
2017-02-13 12:12:26 +0800

    添加oneActitivy

---
+++ ZhaoZhuo_SDK/src/main/java/com/ignite/vision/sdk/OneActivity.java
@@ -0,0 +1,19 @@
+package com.ignite.vision.sdk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @包名: com.ignite.vision.sdk
+ * @创建者: zpeng
+ * @描述: TODO
+ * @更新内容: TODO
+ */
+public class OneActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
